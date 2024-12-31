12a7717d5bd29c8626245579bfca66fa18adf46b
p-RiseDev <dizelano.53.usinger.917@yandex.ru>
2024-12-31 22:54:42 +0300

    Add files via upload

--- working_code_of_bot1.py
+++ working_code_of_bot1.py
@@ -11,10 +11,27 @@ def watching_on_right_now(message):
 
 @bot.message_handler(commands=['support'])
 def questions_for_me(message):
-    audio = open(r'C:/Users/dmitr/Desktop/TeleBot-1/Навигация по тому, как и куда обратиться за помощью.mp3', 'rb')
+    audio = open(r'C:/Users/dmitr/Desktop/T-Bot1/Навигация по тому, как и куда обратиться за помощью.mp3', 'rb')
     bot.send_audio(message.chat.id, audio)
     audio.close()
     bot.send_message(message.chat.id, "Номер телефона: (8)+79539036130\nНаписать во VKонтакте: vk.com/piroschock.frilance.work\nE-Mail Почта: dizelano.53.usinger.917@internet.ru")
+
+@bot.message_handler(commands=['donate'])
+def send_to_me_moneys(message):
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAELTaVndEZeroNvMp5PBQwu92f7-2QvbAAC0QQAAoPGMRNyZrLbxUDCFjYE")
+    bot.send_message(message.chat.id, "С помощью данной функции вы можете отправить денюжку p-Rise.Developer на поддержку и развитие проектов ☺")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    but1 = types.KeyboardButton("Перевести по YooMoney.")
+    but2 = types.KeyboardButton("Отправить в DonationAlerts.")
+    markup.add(but1, but2)
+    bot.send_message(message.chat.id, "Пожалуйста, выберите каким способом\nвы готовы сделать перевод!", reply_markup=markup)
+
+@bot.message_handler(content_types="text2")
+def payments(message):
+    if message.text == ("Перевести по YooMoney."):
+        bot.send_message(message.chat.id, "Перевод по карте: 4100 1189 4595 9520")
+    elif message.text == ("Отправить в DonationAlerts."):
+        bot.send_message(message.chat.id, "Ссылка на сервис: www.donationalerts.com/r/piroschock1")
 
 @bot.message_handler(commands=['start'])
 def welcome_to_my_chat(message):
@@ -26,7 +43,7 @@ def welcome_to_my_chat(message):
     button3 = types.KeyboardButton("3. Мой аккаунт.")
     markup.add(button1, button2, button3)
     bot.send_message(message.chat.id, "Переслать вам ссылочку для вступления?", reply_markup=markup)
-    # sleep(180)
+    sleep(180)
     
     bot.send_photo(message.chat.id, photo="https://i.ytimg.com/vi/WoJwJPFA9us/maxresdefault.jpg")
     markup = types.InlineKeyboardMarkup()
@@ -37,7 +54,7 @@ def welcome_to_my_chat(message):
     markup.add(torrent1, torrent2, torrent3, torrent4)
     bot.send_message(message.chat.id, "Не забудьте подписаться на мои социальные сети:", reply_markup=markup)
     
-@bot.message_handler(content_types="text")
+@bot.message_handler(content_types="text1")
 def younger(message):
     if message.text == ("1. Да, я присоединюсь!"):
         bot.send_video(message.chat.id, "https://img1.picmix.com/output/stamp/thumb/6/7/9/0/2530976_4ce38.gif", None, 'Text')
